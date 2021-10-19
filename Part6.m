edb4e483dd0ebb923326bf49629fef8b1fac87c9
Jof-syntax <jofre.geli.i@estudiantat.upc.edu>
2021-10-19 11:11:05 +0200

    Part 6 solved

--- Part6.m
+++ Part6.m
@@ -1,29 +1,68 @@
 classdef Part6
-    %UNTITLED2 Summary of this class goes here
-    %   Detailed explanation goes here
     
-    properties
-        Property1
+    properties (Access = public)
+        displacement
     end
     
-    methods
-        function obj = Part6(inputArg1,inputArg2)
-            %UNTITLED2 Construct an instance of this class
-            %   Detailed explanation goes here
-            obj.Property1 = inputArg1 + inputArg2;
+    properties (Access = private)
+        CN
+        COOR
+        K
+        Ff
+    end
+    
+    methods (Access = public)
+        
+        function obj = Part6(Nelem)
+            obj.CN = obj.computeCN(Nelem);
+            obj.COOR = obj.computeCOOR(Nelem);
+            obj.K = obj.AssemblyK();
+            obj.Ff = obj.AssemblyF();
+            obj.displacement = obj.solver(Nelem);
         end
         
-        function K = AssemblyK(COOR,CN)
-            nelem = size(CN, 1 );
-            nnode = size(COOR, 1 );
-            nnodeE = size(CN, 2 );
-            K = sparse( nnode , nnode );
-            for e =1: nelem
-                NODOSe = CN( e , : ) ; COOR_e = COOR(NODOSe ) ;
-                he = COOR_e( 2 )-COOR_e( 1 ) ;
-                Ke = 1/he *[ 1 -1; -1 1] ; % algu...;
-                for a = 1: nnodeE
-                    for b = 1:nnodeE
+        function plot(obj)
+            COOR = obj.COOR;
+            Disp = obj.displacement;
+            plot(COOR, Disp);
+        end
+        
+    end
+    
+    methods (Access = private)
+        
+        function CN = computeCN(obj, Nelem)
+            CN = sparse(Nelem, 2);
+            for i = 1:1:Nelem
+                CN(i, 1) = i;
+                CN(i, 2) = i+1;
+            end
+        end
+        
+        function COOR = computeCOOR(obj, Nelem)
+            COOR = sparse(Nelem+1,1);
+            increase = 1/Nelem; %longitud imposed to 1
+            for i=1:1:Nelem
+                COOR(i+1) = increase*i;
+            end
+        end
+        
+        
+        function K = AssemblyK(obj)
+            COOR = obj.COOR;
+            CN = obj.CN;
+            nElem = size(CN, 1 );
+            nNode = size(COOR, 1 );
+            nNodeE = size(CN, 2 );
+            K = sparse( nNode , nNode);
+            AdditionalTerm = obj.OneDGaussquadratureTermK();
+            for e =1: nElem
+                NODOSe = CN( e , : );
+                COOR_e = COOR(NODOSe );
+                he = COOR_e( 2 ) - COOR_e( 1 );
+                Ke = - 1/he*[ 1 -1; -1 1] + he*AdditionalTerm; %PREGUNTAR SIGNES
+                for a = 1: nNodeE
+                    for b = 1:nNodeE
                         A = CN(e,a);
                         B = CN(e,b);
                         K(A,B) = K(A,B) + Ke(a,b);
@@ -31,6 +70,98 @@ classdef Part6
                 end
             end
         end
+        
+        function Ff = AssemblyF(obj)
+            L = 1;
+            g = 0.01;
+            rho = pi^2/L^2;
+            b = g*pi^2/L;
+            COOR = obj.COOR;
+            CN = obj.CN;
+            nElem = size(CN, 1 );
+            nNode = size(COOR, 1 );
+            nNodeE = size(CN, 2 );
+            Ff =zeros( nNode , 1);
+            for e =1: nElem
+                NODOSe = CN( e , : );
+                COOR_e = COOR(NODOSe );
+                he = COOR_e( 2 ) - COOR_e( 1 );
+                Fe = he*obj.COMPUTE_Fe_FORCE(COOR_e( 1 ), COOR_e( 2 ));
+                for a = 1:nNodeE
+                    A = CN(e, a);
+                    Ff(A) = Ff(A) + Fe(a);
+                end
+            end
+            Ff(nNode, 1) = Ff(nNode, 1) - b;
+        end
+        
+        function displacement = solver(obj, Nelem)
+            Ff = obj.Ff;
+            K = obj.K;
+            g = 0.01;
+            R = 1;
+            L = 1:Nelem+1;
+            L(R) = [];
+            KRR = K(R, R);
+            KRL = K(R, L);
+            KLR = K(L, R);
+            KLL = K(L, L);
+            dR = -g;
+            FL = Ff(L,1);
+            dL = KLL\(FL-KLR*dR);
+            displacement = sparse(Nelem +1);
+            displacement(1) = dR;
+            for i = 1:1:Nelem
+                displacement(i+1) = dL(i);
+            end
+        end
+        
     end
+    
+    methods (Access = private, Static)
+        
+        function AdditionalTermK = OneDGaussquadratureTermK()
+            L = 1;
+            rho = pi^2/L^2;
+            xiG(1) = sqrt(3/5);
+            xiG(2) = -sqrt(3/5);
+            xiG(3) = 0;
+            w(1) = 5/9;
+            w(2) = 5/9;
+            w(3) = 8/9;
+            qFun1 = (1-xiG).^2;
+            int1 = w*qFun1';
+            qFun2 = (1-xiG.^2);
+            int2 = w*qFun2';
+            qFun3 = (1+xiG).^2;
+            int3 = w*qFun3';
+            AdditionalTermK = rho*1/8*[ int1 int2 ;
+                int2 int3];
+        end
+        
+        function TermF = COMPUTE_Fe_FORCE(coordx1, coordx2)
+            L = 1;
+            g = 0.01;
+            rho = pi^2/L^2;
+            s = g*rho^2;
+            xiG(1) = 0.339981043584856;
+            xiG(2) = -0.339981043584856;
+            xiG(3) = 0.861136311594053;
+            xiG(4) = -0.861136311594053;
+            w(1) = 0.652145154862546;
+            w(2) = 0.652145154862546;
+            w(3) = 0.347854845137454;
+            w(4) = 0.347854845137454;
+            x = ((1-xiG)*coordx1+(1+xiG)*coordx2);
+            qFun1 = (1-xiG).*x.^2;
+            int1 = w*qFun1';
+            qFun2 = (1+xiG).*x.^2;
+            int2 = w*qFun2';
+            TermF = s*1/16*[int1;
+                int2];
+        end
+        
+    end
+    
 end
 
--- main.m
+++ main.m
@@ -6,26 +6,45 @@ classdef main < handle
     
     properties (Access = private)
         time
+        Nelem
     end
     
     methods (Access = public)
         
         function obj = main()
+            %%%%%%%%%%%%%%%%%%%% Part1 %%%%%%%%%%%%%%%%%%%%
             clc;
             close all;
             obj.createTimeForPlot();
+            obj.computeNelem();
             time = obj.time;
-            a = Part2();
-            a.plot(time);
-            b = Part5(a.solution);
-            b.plot1(time);
-            b.plot2(time);
-            b.plot3(time);
-            b.plot4(time);
+            %a = Part2();
+            %a.plot(time);
+            %b = Part5(a.solution);
+%             b.plot1(time);
+%             b.plot2(time);
+%             b.plot3(time);
+%             b.plot4(time);
+            c = Part6(5);
+            c.plot();
+%             hold on;
+%             c = Part6(10);
+%             c.plot();
+%             c = Part6(20);
+%             c.plot();
+%             c = Part6(40);
+%             c.plot();
+%             legend('Nelem = 5','Nelem = 10', 'Nelem = 20', 'Nelem = 40', 'Fontsize', 14)
+%fer class part 7 que et grafiqui els 4 casos
+              %%%%%%%%%%%%%%%%%%%% Part2 %%%%%%%%%%%%%%%%%%%%
         end
     end
     
     methods (Access = private)
+        
+        function computeNelem(obj)
+            obj.Nelem = 40;
+        end
         
         function createTimeForPlot(obj)
             obj.time = 7;
