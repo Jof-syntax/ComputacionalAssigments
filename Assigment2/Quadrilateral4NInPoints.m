1f096ebc7f83215ec96696b7777030f65b7de285
Jof-syntax <70135822+Jof-syntax@users.noreply.github.com>
2021-11-14 09:55:20 +0100

    function completed

--- Assigment2/Quadrilateral4NInPoints.m
+++ Assigment2/Quadrilateral4NInPoints.m
@@ -1,23 +1,20 @@
-function [weig,posgp,shapef,dershapef] = Quadrilateral4NInPoints
-
+function [weig,posgp,shapef,dershapef] = Quadrilateral4NInPoints()
 weig  = [1 1 1 1] ;
-posgp(1) = 1/sqrt(3)*[-1 -1 ] ;
-posgp(2) = 1/sqrt(3)*[1 -1 ] ;
-posgp(3) = 1/sqrt(3)*[1 1 ] ;
-posgp(4) = 1/sqrt(3)*[-1 1 ] ;
-
-
-ndim = 1;
-nnodeE = 2 ;
+posgp = 1/sqrt(3)*[-1 -1;
+                    1 -1;
+                    1  1;
+                   -1  1];
+ndim = 2;
+nnodeE = 4;
 ngaus = length(weig) ;
-shapef = zeros(ngaus,nnodeE) ;
-dershapef = zeros(ndim,nnodeE,ngaus) ;
-for g=1:length(weig) 
-    xi = posgp(g) ;
-    eta = posgp(g) ;%que es eta?
+shapef = zeros(ngaus, nnodeE) ;
+dershapef = zeros(ndim, nnodeE, ngaus) ;
+for g = 1:length(weig)
+    xi = posgp(g, 1);
+    eta = posgp(g, 2);
     Ne = 1/4*[(1-xi)*(1-eta)  (1+xi)*(1-eta) (1+xi)*(1+eta) (1-xi)*(1+eta)];
     BeXi = 1/4*[-(1-eta)  (1-eta)  (1+eta)  -(1+eta);
-                -(1-xi)  -(1+xi)   (1+xi)    (1-xi)] ;
+        -(1-xi)  -(1+xi)   (1+xi)    (1-xi)] ;
     shapef(g,:) = Ne ;
     dershapef(:,:,g) = BeXi ;
 end
