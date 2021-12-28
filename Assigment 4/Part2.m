abe43ab2f454c2a3a92b4783f1a9011b7baaee5a
Jof-syntax <70135822+Jof-syntax@users.noreply.github.com>
2021-12-28 17:58:33 +0100

    LGTM

--- Assigment 4/Part2.m
+++ Assigment 4/Part2.m
@@ -1,18 +1,19 @@
 clc
 clear
-load('INFO_FE.mat');
-load('dataP4.mat');
-
-neig = 25;
+load('INFO_FE.mat');        %these loads contain some useful information
+load('dataP4.mat');         %such as the COOR and DORr arrays or the stiffness and mass matrices
+neig = 25;                  %number of modes
 
 nnode = size(COOR,1); 
 ndim = size(COOR,2);
 DOFl = 1:nnode*ndim ;
 DOFl(DOFr) = [] ; 
 
-Mll = M(DOFl,DOFl);
+Mll = M(DOFl,DOFl);   % Not restricted matrices of 'M' and 'K'
 Kll = K(DOFl,DOFl);
 
-[MODES, FREQ] = UndampedFREQ(Mll,Kll,neig); % FREQ is w
+[MODES, FREQ] = UndampedFREQ(Mll,Kll,neig);         %using this function
+                                                    %we obtain the natural
+                                                    %frequencies and modes
 
 GidPostProcessModes(COOR,CN,TypeElement,MODES,posgp,NameFileMesh,DATA,DOFl); 
